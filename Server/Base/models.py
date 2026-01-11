d74e027cad3d11510a956057583c6992b6e8bc90
Amadiane <129948408+Amadiane@users.noreply.github.com>
2026-01-11 02:41:18 +0100

    second commit betcom

--- Server/Base/models.py
+++ Server/Base/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db import models
 from django.utils import timezone
 from cloudinary.models import CloudinaryField
+from django.utils import translation
+
 
 
 
